9dcdd7b6c649d1b5e14e3ac9174bb5b07812ef9a
kmranrg <kmranrg@gmail.com>
2022-05-08 19:07:43 +0530

    spirograph final

--- 006_Spirograph/spirograph.py
+++ 006_Spirograph/spirograph.py
@@ -4,12 +4,15 @@ import turtle as t
 # declaring the background color
 t.bgcolor('black')
 
-# setting up the pen size and the speed of the turtle variable
+# setting up the pen size 
 t.pensize(2)
-t.speed(0)
+
+# setting up  the speed of the turtle variable
+t.speed(10)
 
 # defining the color palette
-colors = ['red', 'magenta', 'blue', 'cyan', 'green', 'white', 'yellow']
+colors = ['red','magenta','blue',
+        'cyan','green','white','yellow']
 
 # logic
 for i in range(6):
